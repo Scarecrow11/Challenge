a892993c069ad1e532511d2619ba32ee94f6238b
Scarecrow11 <dimon.smaile1@gmail.com>
2019-06-20 17:08:28 +0300

    Fix import in currencies

--- server/database/currencies.js
+++ server/database/currencies.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { connection, responseDB } from './connection';
-import { checkAuth } from '../utilities/utilities';
+import { checkAuth } from '../utilities/main';
 const currencies = express.Router();
 
 currencies.get('/', checkAuth, (req, res) => {
